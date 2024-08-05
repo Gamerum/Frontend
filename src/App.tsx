d11daecf37d3f398dff9325fb9453f8910e8213d
begumbolatt <bolatbegum98@gmail.com>
2024-08-05 16:53:04 +0300

    wrap App component with BrowserRouter to fix routing issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen text-3xl text-red-500">
-      Welcome
+    <div className="flex items-center justify-center min-h-screen ">
+      <Link to={'/deneme'} className="text-3xl text-red-500">
+        Welcome
+      </Link>
     </div>
   );
 }
