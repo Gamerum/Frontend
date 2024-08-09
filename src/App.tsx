2ed1bc2b89f82d522e52498a265bae7810cb768b
begumbolatt <bolatbegum98@gmail.com>
2024-08-09 17:03:54 +0300

    colors codes and container padding size arrange inside config file

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen ">
-      <Link to={'/deneme'} className="text-3xl text-red-500">
-        Welcome
+      <Link to={'/deneme'} className="text-3xl text-main-500">
+        Welcomes
       </Link>
     </div>
   );
