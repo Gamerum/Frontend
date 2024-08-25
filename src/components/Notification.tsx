e46b53662f63a8d3faa7dd71a2d4cf82e07aaa2a
begumbolatt <bolatbegum98@gmail.com>
2024-08-25 22:15:49 +0300

    otification general panel created

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,18 +1,42 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
 import { Badge } from 'primereact/badge';
+import { TieredMenu } from 'primereact/tieredmenu';
+import { TieredMenu as TieredMenuType } from 'primereact/tieredmenu';
 
 function Notification() {
-  const value: number = 5;
+  const menu = useRef<TieredMenuType>(null);
+  const [notifications, setNotifications] = useState(generateNotifications());
+
+  function generateNotifications() {
+    const randomNotifications = Array.from({ length: 5 }, (_, i) => ({
+      label: `Notification ${i + 1}`,
+      icon: 'pi pi-info-circle',
+      command: () => console.log(`Notification ${i + 1} clicked`),
+    }));
+    return randomNotifications;
+  }
+
+  const items = notifications.map((notification) => ({
+    label: notification.label,
+    icon: notification.icon,
+    command: notification.command,
+  }));
 
   return (
-    <div className="text-2xl">
+    <div className="relative text-2xl">
       <i
         className="pi pi-bell p-overlay-badge text-main-250"
         style={{ fontSize: '1.5rem' }}
+        onClick={(e) => menu.current?.toggle(e)}
       >
-        <Badge value={value}></Badge>
+        <Badge value={notifications.length} />
       </i>
+      <TieredMenu
+        model={items}
+        popup
+        ref={menu}
+        className="notification-menu bg-main-850 border border-secondary-350 rounded-lg text-main-300"
+      />
     </div>
   );
 }
