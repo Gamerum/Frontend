d711138f23980d76256c03d1b1a00d1b1dd682af
Mustafa Toprak <mtoprak.l203@gmail.com>
2024-08-28 20:33:25 +0300

    MemberList added. #11

--- src/components/right_sidebar/community/RuleList.tsx
+++ src/components/right_sidebar/community/RuleList.tsx
@@ -11,7 +11,7 @@ const RuleList: React.FC<RuleListProps> = ({ rules }) => {
       <h2 className="text-xl font-bold mb-3 text-left border-b-2 border-blue-600">
         Rules
       </h2>
-      <ul className="space-y-1 text-sm overflow-y-scroll max-h-52 scrollbar scrollbar-thumb-gray-600 scrollbar-track-transparent scrollbar-thumb-rounded-full">
+      <ul className="space-y-1 text-sm overflow-y-auto max-h-52 scrollbar scrollbar-thumb-gray-600 scrollbar-track-transparent scrollbar-thumb-rounded-full">
         {rules.map((rule, index) => (
           <li key={index} className="p-1 bg-main-800 shadow-md rounded-md">
             {rule}
--- src/layout/community_page/RightSidebar.tsx
+++ src/layout/community_page/RightSidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import RuleList from '../../components/right_sidebar/community/RuleList';
+import MemberList from '../../components/right_sidebar/community/MemberList';
 
 const RightSidebar: React.FC = () => {
   const rules = [
@@ -15,9 +16,37 @@ const RightSidebar: React.FC = () => {
     'No spamming',
   ];
 
+  const members = [
+    {
+      id: '0',
+      pp: 'https://example.com/profile1.jpg',
+      nickname: 'JohnDoe',
+      role: 'Moderator',
+    },
+    {
+      id: '1',
+      pp: 'https://example.com/profile2.jpg',
+      nickname: 'JaneSmith',
+      role: 'Member',
+    },
+    {
+      id: '2',
+      pp: 'https://example.com/profile1.jpg',
+      nickname: 'JohnDoe',
+      role: 'Moderator',
+    },
+    {
+      id: '3',
+      pp: 'https://example.com/profile2.jpg',
+      nickname: 'JaneSmith',
+      role: 'Member',
+    },
+  ];
+
   return (
     <aside className="w-[20rem] bg-zinc-900 h-full flex-col hidden md:flex overflow-visible">
       <RuleList rules={rules} />
+      <MemberList members={members} />
     </aside>
   );
 };
